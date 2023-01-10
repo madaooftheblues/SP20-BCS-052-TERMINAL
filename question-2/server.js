59f98cf0d12183d6b55c760eee248d2def896f53
madaooftheblues <azlanrasool@gmail.com>
2023-01-10 15:03:21 +0500

    Add get methods

--- question-2/server.js
+++ question-2/server.js
@@ -7,3 +7,18 @@ const todos = [
   { content: "Finish reading book", status: false },
   { content: "Take dog for walk", status: true },
 ];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to your todos api");
+});
+
+app.get("/appi/todos", (req, res) => {
+  if (!todos) res.status(400).send("No todos found");
+  res.send(todos);
+});
+
+app.get("/api/todos/:index", (req, res) => {
+  if (!todos[req.params.index])
+    return res.status(400).send("todo does not exist");
+  res.send(todos[req.params.index]);
+});
